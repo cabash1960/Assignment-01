6930c1b146604e8cad46bdf41faeb15e181d907d
cabash1960 <blessingxin1960@gmail.com>
2025-09-24 23:07:21 +0100

    test-run

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-import { calculateFactorials, celsiusToFahrenheit, doubleTheValues, evenNumbersWithin, findMultiples, findPrimeNumbers, oddNumbersWithin, sumOfNumbersTo } from './assignment.js'
+import {
+    calculateFactorials,
+    celsiusToFahrenheit,
+    doubleTheValues,
+    evenNumbersWithin,
+    findMultiples,
+    findPrimeNumbers,
+    oddNumbersWithin,
+    sumOfNumbersTo,
+} from './assignment.js'
 
 if (sumOfNumbersTo && typeof sumOfNumbersTo === 'function') {
     console.log(sumOfNumbersTo(10))
@@ -25,9 +34,78 @@ if (calculateFactorials && typeof calculateFactorials === 'function') {
 }
 
 if (findPrimeNumbers && typeof findPrimeNumbers === 'function') {
-    console.log(findPrimeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+    console.log(
+        findPrimeNumbers([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+        ]),
+    )
 }
 
 if (doubleTheValues && typeof doubleTheValues === 'function') {
-    console.log(doubleTheValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+    console.log(
+        doubleTheValues([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18,
+            19,
+            20,
+        ]),
+    )
 }
+
+// const data = {
+//   results: [
+//     {
+//       value: 709,
+//       correctFunction: {
+//         sum: 125670,
+//         count: 355,
+//         arrayOfEvenNumbers: [],
+//       },
+//     },
+//   ],
+//   info: {
+//     seed: "d9220e5aed0dbc97",
+//     results: "1",
+//     page: "1",
+//     version: "0.1",
+//     time: { instruct: 13, generate: 11 },
+//   },
+// };
+
+// const result = evenNumbersWithin(280);
+// console.log(JSON.stringify(result, null, 2));
